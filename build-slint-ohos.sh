82d20e035926176be4b113fc58bd7d48e1026fef
tzw <tzw@tdcare.cn>
2024-01-29 23:24:46 +0800

    测试 hilog 通过

--- build-slint-ohos.sh
+++ build-slint-ohos.sh
@@ -1,2 +1,2 @@
 docker run --rm -it -v $(pwd):/io -w /io/examples/ohos-demo swr.cn-south-1.myhuaweicloud.com/tdcare/zig-build-slint:v1.70.0 \
-  RUSTFLAGS="-C target-feature=-crt-static" cargo zigbuild --release --target aarch64-unknown-linux-musl
+  export OHOS_NDK_HOME=/io/ohos-sdk/linux/native && RUSTFLAGS="-C target-feature=-crt-static" cargo zigbuild --release --target aarch64-unknown-linux-musl
