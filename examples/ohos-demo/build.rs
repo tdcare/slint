350087b0b051c52a84714e251228ccb101608e98
tzw <tzw@tdcare.cn>
2024-01-30 22:06:26 +0800

    测试 demo

--- examples/ohos-demo/build.rs
+++ examples/ohos-demo/build.rs
@@ -5,11 +5,11 @@ use napi_build_ohos;
 fn main() {
     napi_build_ohos::setup();
 
-    slint_build::compile_with_config(
-        "demo.slint",
-        slint_build::CompilerConfiguration::new()
-            .with_style("fluent-dark".to_owned())
-            .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
-    )
-    .unwrap();
+    // slint_build::compile_with_config(
+    //     "demo.slint",
+    //     slint_build::CompilerConfiguration::new()
+    //         .with_style("fluent-dark".to_owned())
+    //         .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
+    // )
+    // .unwrap();
 }
