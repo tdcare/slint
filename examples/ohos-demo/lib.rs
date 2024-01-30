079d3300ee18934de071a183101bb0bd75a4e5f0
tzw <tzw@tdcare.cn>
2024-01-31 00:39:32 +0800

    debug 4

--- examples/ohos-demo/lib.rs
+++ examples/ohos-demo/lib.rs
@@ -58,7 +58,7 @@ slint::slint!(import { Booker } from "booker.slint";);
 #[no_mangle]
 pub fn init_demo(ohos_widows: *mut c_void,w:u32,h:u32,message:*mut c_char)-> i32 {
     let mut errored=false;
-    let mut message_c_string=CString::new("Running ").expect("Failed to create CString");
+    let mut message_c_string=CString::new(format!("Running ")).expect("Failed to create CString");
 /*
     match   Backend::new(ohos_widows, w, h) {
         Ok(backend) => {
