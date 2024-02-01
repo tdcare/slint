e33b3acc3f2779491cc4ff9c2fb02624e1142702
tzw <tzw@tdcare.cn>
2024-02-02 02:01:28 +0800

    Event Loop

--- internal/backends/ohos/calloop_backend.rs
+++ internal/backends/ohos/calloop_backend.rs
@@ -190,8 +190,9 @@ impl Platform for Backend {
             // return Err(PlatformError::from("调试".to_string()));
 
             if let Some(adapter) = self.window.borrow().as_ref() {
+
                 adapter.render_if_needed(mouse_position_property.as_ref())?;
-                adapter.clone().register_event_loop(event_loop.handle())?;
+                // adapter.clone().register_event_loop(event_loop.handle())?;
             };
             // return Err(PlatformError::from("调试1".to_string()));
 
