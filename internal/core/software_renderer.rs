2237c0c5041a6b53411451922b7f7ba783c3be46
tzw <tzw@tdcare.cn>
2024-02-07 03:08:00 +0800

    软实现方法测试9

--- internal/core/software_renderer.rs
+++ internal/core/software_renderer.rs
@@ -352,14 +352,14 @@ impl SoftwareRenderer {
         if size.is_empty() {
             return Default::default();
         }
-        assert!(
-            if rotation.is_transpose() {
-                pixel_stride >= size.height as usize && buffer.len() >= (size.width as usize * pixel_stride + size.height as usize) - pixel_stride
-            } else {
-                pixel_stride >= size.width as usize && buffer.len() >= (size.height as usize * pixel_stride + size.width as usize) - pixel_stride
-            },
-            "buffer of size {} with stride {pixel_stride} is too small to handle a window of size {size:?}", buffer.len()
-        );
+        // assert!(
+        //     if rotation.is_transpose() {
+        //         pixel_stride >= size.height as usize && buffer.len() >= (size.width as usize * pixel_stride + size.height as usize) - pixel_stride
+        //     } else {
+        //         pixel_stride >= size.width as usize && buffer.len() >= (size.height as usize * pixel_stride + size.width as usize) - pixel_stride
+        //     },
+        //     "buffer of size {} with stride {pixel_stride} is too small to handle a window of size {size:?}", buffer.len()
+        // );
         let buffer_renderer = SceneBuilder::new(
             size,
             factor,
