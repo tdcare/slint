f9055930904e609c81a25c3599b689c7f84e4fb9
tzw <tzw@tdcare.cn>
2024-02-07 01:49:58 +0800

    软实现方法测试5

--- examples/ohos-demo/lib.rs
+++ examples/ohos-demo/lib.rs
@@ -283,7 +283,7 @@ pub fn init_memory(ohos_widows: *mut c_void,w:u32,h:u32,message:*mut c_char)-> i
 }
 // 软实现方案，取数据
 #[no_mangle]
-pub fn slint_buffer(buffer: &mut c_void, message:*mut c_char) ->i32 {
+pub fn slint_buffer(buffer: *mut c_char, message:*mut c_char) ->i32 {
     let mut errored=false;
     let mut message_c_string=CString::new(format!("Running ")).expect("Failed to create CString");
    match   FRAME_BUFFER.get(){
