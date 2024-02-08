027884108ac26ccd068abb5b501cb2b1f4179cd9
tzw <tzw@tdcare.cn>
2024-02-09 00:51:14 +0800

    napi_ohos

--- examples/ohos-demo/build.rs
+++ examples/ohos-demo/build.rs
@@ -1,9 +1,10 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: MIT
-// use napi_build;
+use napi_build_ohos;
 
 fn main() {
     // napi_build::setup();
+    napi_build_ohos::setup();
 
     // slint_build::compile_with_config(
     //     "demo.slint",
