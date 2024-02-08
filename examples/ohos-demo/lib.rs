192ca4d074212c268993ff3a6381c00917296db3
tzw <tzw@tdcare.cn>
2024-02-09 01:23:49 +0800

    napi_ohos

--- examples/ohos-demo/lib.rs
+++ examples/ohos-demo/lib.rs
@@ -72,7 +72,7 @@ pub fn init_demo(ohos_widows: *mut c_void,w:u32,h:u32,message:*mut c_char)-> i32
 
     let mut errored=false;
     let mut message_c_string=CString::new(format!("Running ")).expect("Failed to create CString");
-    hilog_debug!("hello world1");
+    // hilog_debug!("hello world1");
 
     let p=Backend::new(ohos_widows, w, h).unwrap();
     slint::platform::set_platform(Box::new(p)).unwrap();
